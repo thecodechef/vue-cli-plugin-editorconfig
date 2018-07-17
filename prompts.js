0b432f72f87e428d39d2d2c037fc3246330065f2
Jeremy Bolding <cyberchefjay@gmail.com>
2018-07-16 21:02:43 -0500

    :green_heart: Fixed some minor issues

--- prompts.js
+++ prompts.js
@@ -3,28 +3,28 @@ module.exports = [
     name: 'isRoot',
     type: 'confirm',
     message: 'Is this in the root?',
-    default: true,
+    default: true
   },
   {
     name: 'setCharset',
     type: 'list',
     message: 'What is the charset?',
     choices: ['latin1','utf-8','utf-16be','utf-16le','utf-8-bom']
-    default: 'utf-8',
+    default: 'utf-8'
   },
   {
     name: 'setLineEndings',
     type: 'list',
     message: 'Would you like to set the end_of_line option?',
-    choices: ['lf','crlf','cr']
-    default: 'lf',
+    choices: ['lf','crlf','cr'],
+    default: 'lf'
   },
   {
     name: 'setIndentStyle',
     type: 'list',
     message: 'Do you use spaces or tabs?',
-    choices: ['space','tab']
-    default: 'space',
+    choices: ['space','tab'],
+    default: 'space'
   },
   {
     name: 'setIndentSize',
