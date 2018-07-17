fe49bcb948439a5521b90ba34156a23e9928864c
Jeremy Bolding <cyberchefjay@gmail.com>
2018-07-17 05:54:45 +0000

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,11 +3,9 @@ module.exports = {
   title: 'VuePress',
   description: 'A Test Documentation',
   docsDir: 'docs',
-  docsBranch: 'pages',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
       { text: 'External', link: 'https://google.com' },
     ]
   }
