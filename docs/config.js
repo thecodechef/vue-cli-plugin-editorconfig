744263887860c08ba79b15d2684d2de58767dbbc
Jeremy Bolding <cyberchefjay@gmail.com>
2018-07-17 05:21:23 +0000

    Update config.js

---
+++ docs/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dest: 'public',
+  title: 'VuePress',
+  description: 'A Test Documentation',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
